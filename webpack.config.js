ebcf76197e2ff879bba17ed741f3810095bf15f2
Dzianis_Dashkevich <dzianis_dashkevich@epam.com>
2018-09-03 19:15:03 +0300

    eco: add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const { resolve } = require('path');
+
+const src = resolve(__dirname, 'src');
+const indexJs = resolve(src, 'index.js');
+const dist = resolve(__dirname, 'dist');
+
+const entry = indexJs;
+const output = { path: dist, filename: 'bundle.js' };
+
+module.exports = {
+	entry,
+	output,
+	module: {
+		rules: [
+			{
+				test: indexJs,
+				use: 'babel-loader',
+			}
+		]
+	}
+}
+
