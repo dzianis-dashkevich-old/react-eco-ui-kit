161fbae2f98df49877551579ce777b70d6417cd7
Dzianis_Dashkevich <dzianis_dashkevich@epam.com>
2018-09-03 23:43:27 +0300

    eco: add prop types picker

---
+++ src/components/Picker/propTypes/index.js
@@ -0,0 +1,2 @@
+export { default as PICKER_PROP_TYPES_ALL } from './propTypes.js';
+export { default as PICKER_DEFAUL_PROP_TYPES } from './default.js';
---
+++ src/components/Picker/propTypes/propTypes.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export default {
+	disabled: PropTypes.bool,
+	picked: PropTypes.bool,
+	value: PropTypes.any,
+	onPickerClick: PropTypes.func,
+	pickerIndex: PropTypes.any,
+}
