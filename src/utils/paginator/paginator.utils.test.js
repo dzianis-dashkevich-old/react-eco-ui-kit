387473a458e8ebd10cedf151e09297491b19a9f3
Dzianis_Dashkevich <dzianis_dashkevich@epam.com>
2018-09-05 23:44:56 +0300

    eco: update test case

--- src/utils/paginator/paginator.utils.test.js
+++ src/utils/paginator/paginator.utils.test.js
@@ -9,5 +9,12 @@ describe('paginator utils spec', () => {
 			expect(isValueInvalid(undefined)).toBeTruthy();
 			expect(isValueInvalid(false)).toBeTruthy();
 		});
+
+		it('should return false if value is valid', () => {
+			expect(isValueInvalid(1)).toBeFalsy();
+			expect(isValueInvalid(123)).toBeFalsy();
+			expect(isValueInvalid(Infinity)).toBeFalsy();
+			expect(isValueInvalid(true)).toBeFalsy();
+		})
 	});
 });
