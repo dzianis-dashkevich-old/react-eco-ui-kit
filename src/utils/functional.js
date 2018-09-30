dedd7541c481bb957b2d47d8d8765c0c7fad3c63
Dzianis_Dashkevich <dzianis_dashkevich@epam.com>
2018-09-30 21:24:06 +0300

    add functional utils

--- src/utils/functional.js
+++ src/utils/functional.js
@@ -1,1 +1,23 @@
 export const noop = () => {};
+
+export const identity = (arg) => arg;
+
+export const pipe = (...funcs) => {
+	if (!funcs.length) {
+		return identity;
+	}
+
+	if (funcs.length === 1) {
+		return funcs[0]
+	}
+
+	return funcs.reduceRight((a, b) => (...args) => a(b(...args)))
+};
+
+export const assert = (condition, message) => {
+	if (!condition) {
+		throw new Error(`Assertion failed: ${message}`);
+	}
+
+	return condition;
+}
