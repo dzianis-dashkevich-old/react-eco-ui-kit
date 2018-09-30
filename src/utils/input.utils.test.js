71b35ef33a4418096d07100ecf9f91b24d683705
Dzianis_Dashkevich <dzianis_dashkevich@epam.com>
2018-09-30 22:56:26 +0300

    remove files

--- src/utils/input.utils.test.js
+++
@@ -1,9 +0,0 @@
-import { basicNumberValidator } from './index';
-
-describe('input utils spec', () => {
-	describe('basic number validator', () => {
-		it('should return false, if provided value cant cast to number', () => {
-			expect(basicNumberValidator('val')).toBeFalsy();
-		});
-	});
-});
