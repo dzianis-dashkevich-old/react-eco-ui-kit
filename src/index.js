111043452bbcbc2358b47beff0413a37cf3e3459
Dzianis_Dashkevich <dzianis_dashkevich@epam.com>
2018-10-04 18:31:25 +0300

    update index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import Picker from './Picker';
 import Paginator from './Paginator';
 import Input from './Input';
 import Delimeter from './Delimeter';
+import Control from './Control';
+import Label from './Label';
 
 import * as CORE from './consts/core';
 import * as INPUT from './consts/input';
@@ -13,9 +15,10 @@ import * as DELIMETER from './consts/delimeter';
 
 import * as functional from './utils/functional';
 import * as paginator from './utils/paginator';
+import * as string from './utils/string';
 
 export const consts = { CORE, INPUT, PICKER, PAGINATOR, LABELS, CONTROLS, DELIMETER };
-export const utils = { functional, paginator };
-export const components = { Picker, Paginator, Input, Delimeter };
+export const utils = { functional, paginator, string };
+export const components = { Picker, Paginator, Input, Delimeter, Control, Label };
 
 export default { components, utils, consts };
